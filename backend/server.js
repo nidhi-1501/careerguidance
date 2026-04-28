52898dcc86ffdc160368f3d5b6b6de9ae7163970
Nidhi <ramchandaninidhi71@gmail.com>
2026-04-29 00:08:28 +0530

    fixed mongodb env

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connect
-mongoose.connect(mongodb_uri)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
@@ -22,6 +22,7 @@ app.get("/test", (req, res) => {
   res.send("working");
 });
 
-app.listen(5001, () => {
-  console.log("Server running on http://localhost:5001");
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
