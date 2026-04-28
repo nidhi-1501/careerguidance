8b5214e193c7c08780ff1579fd69eb60da916da4
Nidhi <ramchandaninidhi71@gmail.com>
2026-04-29 00:20:29 +0530

    connected backend

--- react-frontend/src/api.js
+++ react-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://careerguidance-oe1v.onrender.com/api",
 });
 
 export default API;
